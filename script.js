ef1db5cf4a34776df3a12ca9ba887759442e2556
Yuliya <vsggdfaf@gmail.com>
2022-01-11 11:31:26 +0300

    обновление в js

--- script.js
+++ script.js
@@ -1,38 +1,101 @@
-
 const appData = {
     title: '',
-    screens: '',
-    screenPrice: '',
+    screens: [],
+    screenPrice: 0,
     adaptive: true,
     rollback: 10,
     allServicePrices: 0,
     fullPrice: 0,
-    servicePercentPrice: 0,
-    service1: '',
-    service2: '',
+    serviePercentPrice: 0,
+    services: {},
     start: function () {
-        appData.asking()
-        appData.allServicePrices = appData.getAllServicesPrices()
-        appData.fullPrice = appData.getFullPrice()
-        appData.servicePercentPrice = appData.getServicePercentPrice()
-        appData.title = appData.getTitle()
-
-        appData.logger()
+      appData.asking()
+      appData.addPrice()
+      appData.getFullPrice()
+      appData.getServicePercentPrices()
+      appData.getTitle()
+      
+      appData.logger()
     },
     isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num)
+      return !isNaN(parseFloat(num)) && isFinite(num)
     },
-    asking: function() {
-        appData.title = promt("Как называется Ваш проект?", "Калькулятор верстки")
-        appData.screens = promt("Какие типы экранов нужно разрабатывать?", "Простые, сложные")
-
+    asking: function () {
+      appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
+      
+      for (let i = 0; i < 2; i++) {
+        let name =  prompt('Какие типы экранов нужно разработать?')
+        let price = 0
+  
         do {
-            appData.screenPrice = promt("Сколько будет стоить Ваша работа?")
-        } while (!appData.isNumber(appData.screenPrice))
-
-        appData.adaptive = confirm("Нужен ли адаптив на сайте?")
-    },
-    getAllServicePrices: function () {
+          price = prompt('Сколько будет стоить данная работа?')
+        } while (!appData.isNumber(price))
+  
+        appData.screens.push({id: i, name: name, price: price})
+      }
+  
+      for (let i = 0; i < 2; i++) {
+        let name = prompt('Какой дополнительный тип услуги нужен?')
+        let price = 0
         
+        do {
+          price = prompt('Сколько это будет стоить?');
+        } while (!appData.isNumber(price))
+  
+        appData.services[name] = +price
+      }
+      
+      appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+    },
+    addPrice: function () {
+      for (let screen of appData.screens) {
+        appData.screenPrice += +screen.price
+      }
+  
+      for(let key in appData.services) {
+        appData.allServicePrices += appData.services[key]
+      }
+    },
+    getFullPrice: function () {
+      appData.fullPrice =  +appData.screenPrice + appData.allServicePrices
+    },
+    getServicePercentPrices: function () {
+      appData.serviePercentPrice =  appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+    },
+    getTitle: function () {
+      appData.title =  appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase()
+    },
+    getRollbackMessage: function(price) {
+      if (price >= 30000) {
+        return "Даем скидку в 10%"
+      } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%"
+      } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена"
+      } else {
+        return "Что-то пошло не так"
+      }
+    },
+    logger: function () {
+      console.log("allServicePrices", appData.allServicePrices);
+  
+      console.log(appData.getRollbackMessage(appData.fullPrice));
+      console.log("title",typeof appData.title);
+      console.log("screenPrice",typeof appData.screenPrice);
+      console.log("adaptive",typeof appData.adaptive);
+  
+      console.log("screens.length",appData.screens.length);
+      console.log("screens",appData.screens);
+      console.log("serviePercentPrice",appData.serviePercentPrice);
+  
+      console.log("Стоимость верстки экранов " + appData.screenPrice + " гривен и Стоимость разработки сайта " + appData.fullPrice + " гривен.");
+      console.log(" ");
+      console.log("↓ Свойства и методы appData ↓");
+      console.log(" ");
+      for (let key in appData) {
+        console.log( "Ключ: " + key + " Значение: " + appData[key] );
+      }
     }
-}
+  }
+  
+  appData.start()
