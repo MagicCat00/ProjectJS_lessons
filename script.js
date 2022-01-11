910762cd30ae526410b9d0e50945f3e0a39964b1
Yuliya <vsggdfaf@gmail.com>
2022-01-11 11:54:23 +0300

    обновление в js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const a = 1
-const b = "Новая ветка lesson 01"
+const b = "Вторая переменная"
+
+const title = "ProjectJS"
+console.log(title);
+console.log(typeof title);
+
+const screens = "Простые, Сложные, Интерактивные"
+console.log(screens);
+console.log(typeof screens);
+console.log(screens.split(", "));
+
+const screenPrice = 15000
+console.log("Стоимость верстки экранов " + screenPrice + "рублей/долларов/гривен/юани");
+
+const rollback = 50
+console.log(rollback);
+
+const fullPrice = 50000
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+console.log(fullPrice * (rollback/100));
+
+
+const adaptive = "true"
+console.log(adaptive);
+console.log(typeof adaptive);
