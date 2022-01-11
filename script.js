a4a95ab3cb1e3bde5e31ae1189fef8b572ce4926
Yuliya <vsggdfaf@gmail.com>
2022-01-11 12:31:27 +0300

    обновление в js

--- script.js
+++ script.js
@@ -1,80 +1,59 @@
-const appData = {
-    title: '',
-    screens: '',
-    screenPrice: '',
-    adaptive: true,
-    rollback: 10,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    service1: '',
-    service2: '',
-    start: function () {
-        appData.asking()
-        appData.allServicePrices = appData.getAllServicePrices()
-        appData.fullPrice = appData.getFullPrice()
-        appData.servicePercentPrice = appData.getServicePercentPrice()
-        appData.title = appData.getTitle()
+"use strict";
 
-        appData.logger()
-    },
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num)
-    },
-    asking: function() {
-        appData.title = prompt('Как называется Ваш проект?', 'Калькулятор верстки')
-        appData.screens = prompt('Какие типы экранов нужно разрабатывать?', 'Простые, сложные')
-
-        do {
-            appData.screenPrice = prompt("Сколько будет стоить Ваша работа?")
-        } while (!appData.isNumber(appData.screenPrice))
-
-        appData.adaptive = confirm("Нужен ли адаптив на сайте?")
-    },
-    getAllServicePrices: function () {
-        let sum = 0
-
-        for (let i = 0; i < 2; i++) {
-            let price = 0
-
-            if (i == 0) {
-                appData.service1 = prompt("Какая дополнительная услуга нужна?")
-            } else if (i == 1) {
-                appData.service2 = prompt("Какая дополнительная услуга нужна?")
-            }
-
-            do {
-                price = prompt("Сколько будет стоить?")
-            } while (!appData.isNumber(price))
-
-            sum += +price
-        }
-
-        return sum
-    },
-    getFullPrice: function () {
-        return +appData.screenPrice + appData.allServicePrices
-    },
-    getServicePercentPrice: function () {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-    },
-    getTitle: function () {
-        return appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase()
-    },
-    getRollbackMessage: function (price) {
-        if (price >= 30000) {
-            return "Скидка 10%"
-        } else if (price >= 15000 && price <30000) {
-            return "Скидка 5%"
-        } else if (price >= 0 && price < 15000) {
-            return "Скидка не будет, т.к. сумма слишком маленькая"
-        } else {
-            return "Ошибка"
-        }
-    },
-    logger: function () {
-        console.log(appData.fullPrice);
-        console.log(appData.servicePercentPrice);
-    }
+const books = document.querySelectorAll('.books'),
+    arrBooks = document.querySelectorAll('.book');
+const arr = Object.keys(arrBooks).sort((prev, next) => {
+    if (arrBooks[prev].firstElementChild.innerText > arrBooks[next].firstElementChild.innerText) { 
+        return 1; }
+    if (arrBooks[prev].firstElementChild.innerText < arrBooks[next].firstElementChild.innerText) { 
+        return -1; }
+});
+for (let i = 0; i< arr.length; i++){
+    books[0].appendChild(arrBooks[arr[i]]);
 }
-appData.start()
+
+document.querySelector('body').style.backgroundImage = 'url(./image/Backgrounds.jpg)'; 
+books[0].children[2].querySelector('h2').querySelector('a').text = 'Книга 3. this и Прототипы Объектов';
+document.querySelector('.adv').remove();
+
+const sortChapter = collection => {
+    const sortElements = arr => {
+        const arrInd = Object.keys(arr).sort((prev, next) => {
+            if (arr[prev].textContent > arr[next].textContent) {
+                return 1; }
+            if (arr[prev].textContent < arr[next].textContent) { 
+                return -1; }
+            });
+        let arrNew = [];
+        for (let i = 0; i < arrInd.length; i++){
+            arrNew.push(arr[arrInd[i]]);
+        }
+        return arrNew;
+    };
+
+    const elem = collection.querySelectorAll('li');
+    let arrChapter = [],
+        arrApp = [];
+    elem.forEach(el => {
+        if (el.textContent.indexOf('Введение') > -1) {
+            collection.insertBefore(el, elem[0]);
+        }
+        if (el.textContent.indexOf('Предисловие') > -1) {
+            collection.insertBefore(el, elem[1]);
+        }
+        if (el.textContent.indexOf('Глава') > -1) { arrChapter.push(el); }
+        if (el.textContent.indexOf('Приложение') > -1) { arrApp.push(el); }
+    });
+    arrChapter = sortElements(arrChapter);
+    arrChapter.forEach(el => { collection.appendChild(el); });
+    arrApp = sortElements(arrApp);
+    arrApp.forEach(el => { collection.appendChild(el); });
+};
+
+sortChapter(books[0].children[1].querySelector('ul'));
+sortChapter(books[0].children[4].querySelector('ul'));
+
+let child = document.createElement('li');
+child.innerText = 'Глава 8: За пределами ES6';
+books[0].children[5].querySelector('ul').appendChild(child);
+sortChapter(books[0].children[5].querySelector('ul'));
